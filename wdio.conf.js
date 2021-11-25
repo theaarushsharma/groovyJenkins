c38b5fe7d13cfb91353ef600e4518fc0c510c743
aarush sharma <ps2022497@gmail.com>
2021-11-25 13:02:38 +0530

    changed reporter directory

--- wdio.conf.js
+++ wdio.conf.js
@@ -132,7 +132,21 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec', 'junit'],
+    reporters: [
+        [
+            'spec',
+            {
+                outputDir: 'spec-report',
+            },
+        ],
+    
+        [
+            'junit',
+            {
+                outputDir: './report',
+            },   
+        ],
+    ],
 
 
     
